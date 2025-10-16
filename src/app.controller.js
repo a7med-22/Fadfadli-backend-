587283538b2af16c4749b7c374ebda1586a51836
Ahmed osama <ao6155518@gmail.com>
2025-10-16 20:26:33 +0300

    test get users

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import path from "node:path";
 import checkConnectionDB from "./DB/connection.js";
+import userModel from "./DB/models/user.model.js";
 import { default as userRouter } from "./modules/user/user.controller.js";
 import { globalErrorHandling } from "./utils/response.js";
 const bootstrap = (app, express) => {
@@ -15,6 +16,16 @@ const bootstrap = (app, express) => {
   });
 
   app.use("/users", userRouter);
+
+  // Admin-only endpoint to list all users (mounted at /users)
+  app.get("/users", async (req, res, next) => {
+    try {
+      const users = await userModel.find({}, { password: 0, oldPasswords: 0 });
+      return res.json({ message: "users fetched successfully", data: users });
+    } catch (err) {
+      next(err);
+    }
+  });
 
   app.use("{/*demo}", (req, res, next) => {
     throw new Error(`404 url not found ${req.originalUrl}`, { cause: 404 });
